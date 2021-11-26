609ee84664b73aa27f94b36aca821a96fe5e386f
Jakub Ciszek <jakub@ratingcaptain.com>
2021-11-26 09:58:55 +0100

    Docstrings

--- context_logger/logger.py
+++ context_logger/logger.py
@@ -6,7 +6,15 @@ MODULE_KEY = 'module'
 
 
 class ContextLogger:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, context: Optional[dict[str, Any]]) -> None:
+        """Creates a new instance of ContextLogger.
+
+        If context is passed, a copy if it is used for the new logger.
+
+        Args:
+            name (str): Name of the logger
+            context (Optional[dict[str, Any]]): Context of another logger. Optional.
+        """
         self.logger = logging.getLogger(name)
 
         if context is None:
@@ -17,13 +25,25 @@ class ContextLogger:
 
     @staticmethod
     def setup_logging(level=logging.INFO) -> None:
+        """Configures global options for logging format and level.
+
+        Args:
+            level ([type], optional): Default level of logging. Defaults to INFO.
+        """
         logging.basicConfig(
             format=f'%(levelname)s{LOG_SEPARATOR}%(message)s',
             level=level,
         )
 
-    def add_context(self, key, value) -> None:
-        if key in self.context:
+    def add_context(self, key: str, value: Any, warn_on_existing: bool = True) -> None:
+        """Adds new value to the context.
+
+        Args:
+            key (str): Value name in the context. Must be unique.
+            value (Any): Value added to the context.
+            warn_on_existing (bool, optional): If warning should be logged if the key already exists in the context. Defaults to True.
+        """
+        if warn_on_existing and key in self.context:
             self.warning(
                 f'Setting existing key in context, key={key} with value={self.context[key]} -> {value}'
             )
@@ -34,17 +54,37 @@ class ContextLogger:
         return f'{self.context}{LOG_SEPARATOR}{msg}'
 
     def debug(self, msg: str, **kwargs) -> None:
+        """Logs debug output.
+
+        Args:
+            msg (str): Log message.
+        """
         context_msg = self._add_context_to_msg(msg)
         self.logger.debug(context_msg, **kwargs)
 
     def info(self, msg: str, **kwargs) -> None:
+        """Logs info output.
+
+        Args:
+            msg (str): Log message.
+        """
         context_msg = self._add_context_to_msg(msg)
         self.logger.info(context_msg, **kwargs)
 
     def warning(self, msg: str, **kwargs) -> None:
+        """Logs warning output.
+
+        Args:
+            msg (str): Log message.
+        """
         context_msg = self._add_context_to_msg(msg)
         self.logger.warning(context_msg, **kwargs)
 
     def error(self, msg: str, **kwargs) -> None:
+        """Logs error output.
+
+        Args:
+            msg (str): Log message.
+        """
         context_msg = self._add_context_to_msg(msg)
         self.logger.error(context_msg, **kwargs)
